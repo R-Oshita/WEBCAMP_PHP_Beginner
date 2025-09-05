d1d3fdbfff30f37ead4e04bfa472f858a39d0974
Ubuntu <ubuntu@ip-172-31-16-105.ap-northeast-1.compute.internal>
2025-09-05 09:02:32 +0000

    例外処理

---
+++ multi_catch.php
@@ -0,0 +1,24 @@
+<?php
+
+class MyException extends Exception {}
+
+try {
+
+    // throw new \MyException("自力で投げた例外");
+    // throw new TypeError("自力で投げたtype error");
+    throw new Exception("自力で投げた exception");
+} catch (MyException $e) {
+    echo "MyException をキャッチしました \n";
+    var_dump($e->getMessage());
+} catch (\Exception $e) {
+    echo "Exception をキャッチしました \n";
+    var_dump($e->getMessage());
+} catch (\Error $e) {
+    echo "Error をキャッチしました \n";
+    var_dump($e->getMessage());
+} catch (\Throwable $e) {
+    echo "Throwable をキャッチしました \n";
+    var_dump($e->getMessage());
+}
+
+echo "fin. \n";
