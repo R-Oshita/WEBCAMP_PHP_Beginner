ffbc6157ec888089ae7b40967ed37bbf58f3b3f3
Ubuntu <ubuntu@ip-172-31-16-105.ap-northeast-1.compute.internal>
2025-09-07 13:41:49 +0000

    課題提出0回目_フィボナッチ数

--- PHP_kadai/fibonacci.php
+++ PHP_kadai/fibonacci.php
@@ -1,2 +1,21 @@
-<?php 
+<?php
 
+
+
+$array = [0, 1];
+
+
+// var_dump($array);
+
+$i = 2;
+$sum = 1;
+while ($sum < 10000) {
+
+    $array[] = $array[$i - 2] + $array[$i - 1];
+    $sum = $array[$i];
+    ++$i;
+}
+
+foreach ($array as $value) {
+    echo "{$value} \n";
+}
